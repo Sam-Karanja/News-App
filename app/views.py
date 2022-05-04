adb55e45d2b16b99e60b52b83bb908ef7f32a75a
Moringa School <karanjasam210@gmail.com>
2022-05-04 14:07:04 +0300

    get top headlines news

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from.request import get_news
 
 # Views
 @app.route('/')
@@ -8,12 +9,17 @@ def index():
   View root page function that returns the index page and its data
   '''
   title = "Sam News App"
-  return render_template('index.html', title = title)
+  return render_template('index.html', title = title,top_headlines = top_headlines)
 
 @app.route('/news/<id>')
 def news(id):
   '''
   View news page function that returns the news articles pages and its data
   '''
+tle =  "Sam News App"
+  #Getting top headline news
+  top_headlines = get_news('top-headlines')
+  print(top-headlines)
+  ti
 
   return render_template('news.html', id=id) 
