68ca8d04a55ca9a2652c63906b2416619d6da426
Moringa School <karanjasam210@gmail.com>
2022-05-04 13:45:17 +0300

    add a News class

---
+++ app/models/news.py
@@ -0,0 +1,15 @@
+from inspect import isdatadescriptor
+
+class News:
+    '''
+    News class to define news objects
+    '''
+
+    def __init__(self,id, name, author, title, description,url, urToImage, time)
+        self.id = id
+        self.name = name
+        self.author = author
+        self.title = title
+        self.description = description
+        self.urlToImage = urlToImage
+        self.time = time
--- app/news-test.py
+++ app/news-test.py
@@ -11,7 +11,11 @@ class NewsTest (unittest.TestCase):
         '''
         set up method that will run before every test
         '''
-        self.new_news = News(1234, "Flask has evolved", "A thrilling article on the evolution of flask", "https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/PZDEVWWJ6QI6ZN7OOTYJ3AT4UY.jpg&w=1440" )
+        self.new_news = News(1234, "Flask", "Flask Author", "The Evolution of Flask", "The Story of how Flask Evolved", "https://www.spiegel.de/wirtschaft/unternehmen/adler-group-aktie-von-immobilienkonzern-stuerzt-ab-pruefer-verweigern-testat-a-1f3d36b6-3772-44d8-b5c7-87eaedd4a301", "https://cdn.prod.www.spiegel.de/images/5a0e3272-55fa-43b3-b2bd-93a14b246bc8_w1280_r1.77_fpx50.67_fpy50.jpg", "2022-05-02T08:48:58Z")
+
+
+  def test_instance(self):
+
 
 
         if __name__ == '__main__':
